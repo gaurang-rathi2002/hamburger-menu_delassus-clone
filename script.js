58dae7fa89c32c406c3c1906542b5d1c9d9b9373
gaurang-rathi2002 <82335020+gaurang-rathi2002@users.noreply.github.com>
2021-04-18 00:19:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let element = document.getElementsByClassName('outputimage');
+
+function fin12() {
+    
+    element[0].style.backgroundImage = "url('main12.PNG')" 
+}
+
+function fun12() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+
+function fin13() {
+    
+    element[0].style.backgroundImage = "url('main13.png')" 
+}
+
+function fun13() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin14() {
+    
+    element[0].style.backgroundImage = "url('main14.PNG')" 
+}
+function fun14() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin21() {
+    
+    element[0].style.backgroundImage = "url('main21.png')" 
+}
+
+function fun21() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin22() {
+    
+    element[0].style.backgroundImage = "url('main22.png')" 
+}
+
+function fun22() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin23() {
+    
+    element[0].style.backgroundImage = "url('main23.png')" 
+}
+
+function fun23() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin24() {
+    
+    element[0].style.backgroundImage = "url('main24.png')" 
+}
+
+function fun24() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
+function fin25() {
+    
+    element[0].style.backgroundImage = "url('main25.png')" 
+}
+
+function fun25() {
+    
+     element[0].style.backgroundImage = "url('main1.PNG')" 
+}
